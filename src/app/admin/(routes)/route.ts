42b2573c99758759097a5c8cfbfdec1819b43ecd
Михаел Йорданов <gaws@mail.bg>
2025-04-04 08:51:00 +0300

    fix dashboard bug

--- src/app/admin/(routes)/route.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-export async function GET(request: Request) {
-  //const { data: { session } } = await supabase.auth.getSession();
-
-  
-    redirect("/admin/dashboard");
-  
-}
